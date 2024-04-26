1940ff175281e17667c37f9cc495f4a1d51f9085
David Auk <auk.david@gmail.com>
2024-04-26 09:27:57 +0200

    Check window

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
   const [username, setUsername] = useState(getUsername())
 
   useEffect(() => {
+    if (!window) return
     window.localStorage.setItem('username', username)
   }, [username])
 
